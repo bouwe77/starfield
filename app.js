10efe5401091e7580097d8fe1f30dc9746964f59
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-09-23 21:28:52 +0200

    .

--- app.js
+++ app.js
@@ -10,58 +10,60 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 */
 
-var numStars = 500;
+var numStars = 50;
 var stars = [];
-var size = 1;
+var size = 0.2;
 var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
 
-var speed = 5;
+var speed = 10;
 
+const randomPlanetIndex = 48;
 for (var i = 0; i < numStars; i++) {
-    stars[i] = new Star();
+  stars[i] = new Star(i === randomPlanetIndex ? 20 : size);
 }
 
-function Star() {
-    this.x = Math.random() * canvas.width;      // x location
-    this.y = Math.random() * canvas.height;     // y location
-    this.z = Math.random() * canvas.width;      // z location (depth of star)
+function Star(size) {
+  this.x = Math.random() * canvas.width; // x location
+  this.y = Math.random() * canvas.height; // y location
+  this.z = Math.random() * canvas.width; // z location (depth of star)
 
-    this.move = function() {
-        this.z = this.z - speed;
-        if (this.z <= 0) {
-            this.z = canvas.width;
-        }
+  this.move = function () {
+    this.z = this.z - speed;
+    if (this.z <= 0) {
+      this.z = canvas.width;
     }
+  };
 
-    this.show = function() {
-        var x, y, s;
-        x = (this.x - centerX) * (canvas.width / this.z);
-        x = x + centerX;
+  this.show = function () {
+    var x, y, s;
+    x = (this.x - centerX) * (canvas.width / this.z);
+    x = x + centerX;
 
-        y = (this.y - centerY) * (canvas.width / this.z);
-        y = y + centerY;
+    y = (this.y - centerY) * (canvas.width / this.z);
+    y = y + centerY;
 
-        s = size * (canvas.width / this.z);
+    s = size * (canvas.width / this.z);
 
-        c.beginPath();
-        c.fillStyle = "white";
-        c.arc(x, y, s, 0, Math.PI * 2);
-        c.fill();
-    }
+    c.beginPath();
+    c.fillStyle = "white";
+    c.arc(x, y, s, 0, Math.PI * 2);
+    c.fill();
+  };
 }
 
 function draw() {
-    c.fillStyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < numStars; i++) {
-        stars[i].show();
-        stars[i].move();
-    }
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < numStars; i++) {
+    stars[i].show();
+    stars[i].move();
+  }
 }
 
 function update() {
-    draw();
-    window.requestAnimationFrame(update);
+  draw();
+  window.requestAnimationFrame(update);
 }
+
 update();
