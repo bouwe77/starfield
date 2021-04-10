5f7451551b6f87377678ca456eb4789118ac7132
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-04-10 14:02:16 +0200

    .

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 
-canvas.width = 1200;
-canvas.height = 700;
-
 /*
 For fullscreen:
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 */
-
-var numStars = 50;
-var stars = [];
-var size = 0.2;
+// constants
+canvas.width = 1200;
+canvas.height = 700;
+var numStars = 100;
+//var size = 0.2; //TODO use random sizes between 0.1 and 0.5
 var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
+var speed = 10; // For size 0.5, speeds between 1 and 40 look good, faster than that doesn't.
 
-var speed = 20; // speeds between 1 and 20 look good, faster than that doesn't.
+// useEffect + state
+var stars = [];
 
 const randomPlanetIndex = 123123; // disabled for now
+const planetSize = 20;
+const weightedStarSizes = Array(1)
+  .fill(0.1)
+  .concat(Array(2).fill(0.2))
+  .concat(Array(3).fill(0.3))
+  .concat(Array(4).fill(0.4))
+  .concat(Array(5).fill(0.5));
 for (var i = 0; i < numStars; i++) {
-  stars[i] = new Star(i === randomPlanetIndex ? 20 : size);
+  stars[i] = new Star(
+    i === randomPlanetIndex
+      ? planetSize
+      : weightedStarSizes[Math.floor(Math.random() * weightedStarSizes.length)]
+  );
 }
 
+update();
+
+// functions
 function Star(size) {
   this.x = Math.random() * canvas.width; // x location
   this.y = Math.random() * canvas.height; // y location
@@ -65,5 +79,3 @@ function update() {
   draw();
   window.requestAnimationFrame(update);
 }
-
-update();
